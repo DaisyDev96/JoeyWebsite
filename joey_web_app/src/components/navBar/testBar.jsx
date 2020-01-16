8d9b35643925b5fbc92687acc0c56afd7df03176
daetriana <daetiana@umich.edu>
2020-01-16 15:24:04 -0500

    fixed responsive style on navbar compoent

--- joey_web_app/src/components/navBar/testBar.jsx
+++ joey_web_app/src/components/navBar/testBar.jsx
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 
 export const NavigationBar = () => (
 <Styles>
-<Navbar expand="md">
+<Navbar expand="lg">
     <Navbar.Brand href="/">Joey Tre' </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+    <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ml-auto">
         <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
         <Nav.Item><Nav.Link href="/music">Music</Nav.Link></Nav.Item>
@@ -21,7 +21,7 @@ export const NavigationBar = () => (
 )
 
 const Styles = styled.div`
-.navbar { background-color: hsla(0, 0%, 20%,1)   }
+.navbar { background-color: hsla(0, 0%, 10%,1)   }
 a, .navbar-nav, .navbar-light .nav-link {
 color: red;
 font-size: 1.5em;
@@ -31,9 +31,6 @@ font-size: 1.5em;
 font-size: 2.4em;
 color: red;
 &:hover { color: white; }
-}
-ml-auto{
-    background-color: 'red'
 }
 
 `;
